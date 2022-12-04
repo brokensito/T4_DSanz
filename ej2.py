0b74e2addf8ce1c404464be47f62f257dbcb1f3e
David Sanz <dsanzfuertes.15@gmail.com>
2022-12-04 13:53:02 +0100

    ej 2 diagrama y codigo

---
+++ ej2.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+class Mision():
+    def __init__(self, mision,tipo, planeta, general):
+        self.mision = mision
+        self. tipo = tipo
+        self.planeta = planeta
+        self.general = general
+
+
+def asignacion(datos):
+
+    vehiculos1 = ["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST"]
+    vehiculos2 = vehiculos1 + [ "AT-M6", "AT-MP", "AT-DT"]
+
+    for c in datos:
+
+        if c.general == "Darth Vader" or c.general == "Palpatine":
+            asignacion = ("La asignacion para la mision", c.mision, "sera manual.")
+            
+
+        else:
+            if c.tipo=="Exploracion":
+                asignacion = "15 scout troopers y 2 speeder bike."
+
+            elif c.tipo == "Contencion":
+                asignacion = "30 storm troopers y 3 vehiculos ( ", np.random.choice(vehiculos1, 3), " )."
+
+            elif c.tipo == "Ataque":
+                asignacion = "50 storm troopers y 7 vehiculos ( ", np.random.choice(vehiculos2,7), " )."
+
+        if prioridad !=0:
+            asignacion = ("La asignacion para la mision", c.mision, "sera manual.")
+
+        else:
+            pass
+
+    return asignacion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+
+    misiones = [Mision("Palpatine")]
+
+
