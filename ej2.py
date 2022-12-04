ec4bd07ee98c83de861f24531bc37300c10ad413
David Sanz <dsanzfuertes.15@gmail.com>
2022-12-04 14:23:34 +0100

    ej2 resolucion

--- ej2.py
+++ ej2.py
@@ -13,29 +13,51 @@ def asignacion(datos):
     vehiculos1 = ["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST"]
     vehiculos2 = vehiculos1 + [ "AT-M6", "AT-MP", "AT-DT"]
 
+    resultados = {}
+
     for c in datos:
 
         if c.general == "Darth Vader" or c.general == "Palpatine":
-            asignacion = ("La asignacion para la mision", c.mision, "sera manual.")
+            resultados[c.mision] = ("Asignacion manual.")
             
-
         else:
             if c.tipo=="Exploracion":
-                asignacion = "15 scout troopers y 2 speeder bike."
+                resultados[c.mision] = 15, "scout troopers y", 2 ," speeder bike."
 
             elif c.tipo == "Contencion":
-                asignacion = "30 storm troopers y 3 vehiculos ( ", np.random.choice(vehiculos1, 3), " )."
+                resultados[c.mision] = 30, "storm troopers y", 3 ," vehiculos ( ", np.random.choice(vehiculos1, 3), " )."
 
             elif c.tipo == "Ataque":
-                asignacion = "50 storm troopers y 7 vehiculos ( ", np.random.choice(vehiculos2,7), " )."
+                resultados[c.mision] = 50, "storm troopers y", 7, " vehiculos ( ", np.random.choice(vehiculos2,7), " )."
 
-        if prioridad !=0:
-            asignacion = ("La asignacion para la mision", c.mision, "sera manual.")
+        pregunta = input("¿\nDesea seguir (SI) o agregar una nueva mision (NO) ? ")
 
-        else:
+        if pregunta == "SI":
             pass
 
-    return asignacion
+        else:
+            mision = input("Introduzca el nombre de la mision.")
+            tipo = input("Introduzca el tipo de la mision.")
+            planeta = input("Introduza el destino de la mision.")
+            general = input("Introduzca quien le ecomiendo la mision.")
+            datos.append(Mision(mision, tipo, planeta, general))
+
+
+
+    return resultados
+
+def recursos(datos):
+
+    valores = []
+    for i in datos:
+        
+
+
+if __name__ == "__main__":
+
+
+
+
 
 
 
